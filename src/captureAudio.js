7acf4be7d9f335402d7acd90b90585f5927ad982
Mattias Wadman <mattias.wadman@gmail.com>
2016-10-03 22:05:35 +0200

    Maybe workaround for chrome audio crash

--- src/captureAudio.js
+++ src/captureAudio.js
@@ -33,47 +33,55 @@ function flattenChannelBuffersToSign16(floatArrays) {
   return int16Arrays;
 }
 
-function captureAudio(captureDuration) {
-  return new Promise((resolve, _reject) => {
-    // tabCapture assumes active tab
-    chrome.tabCapture.capture({audio: true}, stream => {
-      // TODO: hack to continue playing. how to remove? how to only create one?
-      let audio = document.createElement('audio');
-      audio.src = window.URL.createObjectURL(stream);
-      audio.play();
+const captureAudio = (() => {
+  let audio;
 
-      let context = new AudioContext();
-      let sampleRate = context.sampleRate;
-      let audioInput = context.createMediaStreamSource(stream);
-
-      const bufferSize = 16384;
-      const channels = 2;
-      let recorder = context.createScriptProcessor(bufferSize, channels, channels);
-
-      let channelBuffers = [];
-      for (let i = 0; i < channels; i++) {
-        channelBuffers[i] = [];
-      }
-
-      recorder.onaudioprocess = e => {
-        for (let i = 0; i < e.inputBuffer.numberOfChannels; i++) {
-          // make copy as buffers seems to be reused
-          channelBuffers[i].push(new Float32Array(e.inputBuffer.getChannelData(i)));
+  return (captureDuration) => {
+    return new Promise((resolve, _reject) => {
+      // tabCapture assumes active tab
+      chrome.tabCapture.capture({audio: true}, stream => {
+        // TODO: hack to continue playing
+        // TODO: i suspect there is a chrome crash bug related to the hack if
+        // many audio tags are created so create only one
+        if (!audio) {
+          audio = document.createElement('audio');
         }
-      };
+        audio.src = window.URL.createObjectURL(stream);
+        audio.play();
 
-      // connect recorder
-      audioInput.connect(recorder);
-      recorder.connect(context.destination);
+        let context = new AudioContext();
+        let sampleRate = context.sampleRate;
+        let audioInput = context.createMediaStreamSource(stream);
 
-      setTimeout(() => {
-        stream.getTracks().forEach(t => t.stop());
-        context.close();
-        resolve({
-          sampleRate: sampleRate,
-          channels: flattenChannelBuffersToSign16(channelBuffers)
-        });
-      }, captureDuration);
+        const bufferSize = 16384;
+        const channels = 2;
+        let recorder = context.createScriptProcessor(bufferSize, channels, channels);
+
+        let channelBuffers = [];
+        for (let i = 0; i < channels; i++) {
+          channelBuffers[i] = [];
+        }
+
+        recorder.onaudioprocess = e => {
+          for (let i = 0; i < e.inputBuffer.numberOfChannels; i++) {
+            // make copy as buffers seems to be reused
+            channelBuffers[i].push(new Float32Array(e.inputBuffer.getChannelData(i)));
+          }
+        };
+
+        // connect recorder
+        audioInput.connect(recorder);
+        recorder.connect(context.destination);
+
+        setTimeout(() => {
+          stream.getTracks().forEach(t => t.stop());
+          context.close();
+          resolve({
+            sampleRate: sampleRate,
+            channels: flattenChannelBuffersToSign16(channelBuffers)
+          });
+        }, captureDuration);
+      });
     });
-  });
-}
+  };
+})();
